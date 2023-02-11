df2b961a56149a7805a7c585a817b6afad65f8d8
Daniel <denamudu@bariansystems.com>
2023-02-12 00:04:35 +0100

    Done with the MovieLand project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,71 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import MovieCard from './MovieCard';
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_KEY = 'a0eab34e';
+const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchCount, setSearchCount] = useState(0);
+
+  const searchMovies = async title => {
+    const response = await axios(`${API_URL}&s=${title}`);
+    setMovies(response.data.Search);
+    setSearchCount(prevSearchCount => prevSearchCount + 1);
+  }
+
   return (
-    <h1>App</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Seach for movies" 
+          type="text"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <img 
+          src={SearchIcon} 
+          alt="search" 
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              {
+                movies?.map(movie => (
+                  <MovieCard 
+                    movie={movie} 
+                    key={movie.imdbID}
+                  />
+                ))
+              }
+            </div>
+          ) : (
+            <div className="empty">
+              <h2>
+                {
+                  searchCount === 0
+                  ?
+                  'Search movies above'
+                  :
+                  'No movies found'
+                }
+              </h2>
+            </div>
+          )
+      }
+    </div>
   )
 }
 
