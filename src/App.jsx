d5576e2c3d96b868cf5806a9e4f9c6b6ba085925
Daniel <denamudu@bariansystems.com>
2023-02-12 13:56:13 +0100

    Updated the API URL to load over HTTPS

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 
 const API_KEY = 'a0eab34e';
-const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
+const API_URL = `https://www.omdbapi.com?apikey=${API_KEY}`;
 
 const App = () => {
   const [movies, setMovies] = useState([]);
